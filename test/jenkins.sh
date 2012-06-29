b92c782dda93b2dfa03a481df0b36cd76e53dd6b
Fil Maj <maj.fil@gmail.com>
2012-06-29 14:42:16 -0700

    parsing through connected android device ids and installing/running tests

--- test/jenkins.sh
+++ test/jenkins.sh
@@ -6,10 +6,13 @@ MOBILE_SPEC=/Users/fil/src/incubator-cordova-mobile-spec
 ANDROID_SDK=/Users/fil/SDKs/android-sdk-macosx
 ADB=$ANDROID_SDK/platform-tools/adb
 VERSION=`cat $ANDROID_REPO/version`
+TEST_PACKAGE="org.apache.cordova.test/android.test.InstrumentationTestRunner"
+
+# List of connected android devices
+IDs=`$ADB devices | grep device$ | awk '{print $1}'`
 
 # If we are this far it is assumed that the jar was compiled
-# successfully
-
+# successfully - first step in jenkins does this
 mkdir $ANDROID_TEST/assets/www/mobilespec
 
 # 1. Copy the jar into the test directory
@@ -29,7 +32,15 @@ cp -rf $MOBILE_SPEC/a* $MOBILE_SPEC/b* $MOBILE_SPEC/c* $MOBILE_SPEC/e* $MOBILE_S
 # 5. Compile the app
 cd $ANDROID_TEST && ant debug
 
-exit 0;
-
-/Users/fil/SDKs/android-sdk-macosx/platform-tools/adb shell am instrument -w org.apache.cordova.test/android.test.InstrumentationTestRunner && \
-/Users/fil/SDKs/android-sdk-macosx/platform-tools/adb pull /mnt/sdcard/Android/data/org.apache.cordova.test/cache ${WORKSPACE}/${JOB_NAME}/xmls
+for i in $IDs
+do
+  # make a directory for the device id
+  rm -rf $WORKSPACE/$JOB_NAME/xml/$i
+  mkdir $WORKSPACE/$JOB_NAME/xml/$i
+  # install the test apk to each device
+  $ADB -s $i install $ANDROID_TEST/bin/tests-debug.apk
+  # run on each device
+  $ADB -s $i shell am instrument -w $TEST_PACKAGE
+  # pull the xml into workspace
+  $ADB -s $i pull /mnt/sdcard/Android/data/org.apache.cordova.test/cache $WORKSPACE/$JOB_NAME/xml
+done
